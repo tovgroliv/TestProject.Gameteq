52c980007e0cef3c8cc17a7d6b3e421b8660ba7f
Maksim <112928024+tovgroliv@users.noreply.github.com>
2025-04-29 19:44:07 +0400

    add command line arguments

--- TestProject.Gameteq.Server/Program.cs
+++ TestProject.Gameteq.Server/Program.cs
@@ -1,7 +1,20 @@
 ﻿using System.Net;
 using TestProject.Gameteq.Server;
 
+string ipAdressArg = args.Length > 0 ? args[0] : "127.0.0.1";
+string portArg = args.Length > 1 ? args[1] : "3333";
+
+if (!IPAddress.TryParse(ipAdressArg, out IPAddress? ipAddress))
+{
+    ipAddress = IPAddress.Parse("127.0.0.1");
+}
+
+if (!int.TryParse(portArg, out int port) || port < 1 || port > 65535)
+{
+    port = 3333;
+}
+
 Console.Clear();
 
 var server = new TcpChatServer();
-await server.InitializeAsync(IPAddress.Parse("127.0.0.1"), 3333);
+await server.InitializeAsync(ipAddress, port);
